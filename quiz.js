31fa8e2d425de894ba8f903213a5e52160384726
fade <565690948@qq.com>
2026-04-10 19:57:19 +0800

    v2 quiz system
    Add quiz page
    Add question bank
    Add interactive feedback
    Add score tracking

--- quiz.js
+++ quiz.js
@@ -1,19 +1,90 @@
-function checkAnswer() {
+let questions = [
+{
+question: "What is the angle in a semicircle?",
+answers: ["90°", "60°", "180°", "45°"],
+correct: 0
+},
 
-let options = document.getElementsByName("q1");
-let answer = "";
+{
+question: "Angles in the same segment are?",
+answers: ["Equal", "Different", "Always 90°", "Random"],
+correct: 0
+},
 
-for (let i = 0; i < options.length; i++) {
-if (options[i].checked) {
-answer = options[i].value;
+{
+question: "The angle at the center is how many times the angle at the circumference?",
+answers: ["2 times", "3 times", "Same", "Half"],
+correct: 0
+}
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+function loadQuestion(){
+
+let q = questions[currentQuestion];
+
+document.getElementById("question").innerText = q.question;
+
+let answersHTML = "";
+
+for(let i=0;i<q.answers.length;i++){
+
+answersHTML += `
+<label>
+<input type="radio" name="answer" value="${i}">
+${q.answers[i]}
+</label><br>
+`;
+
+}
+
+document.getElementById("answers").innerHTML = answersHTML;
+
+}
+
+function submitAnswer(){
+
+let options = document.getElementsByName("answer");
+
+let selected = -1;
+
+for(let i=0;i<options.length;i++){
+if(options[i].checked){
+selected = options[i].value;
 }
 }
 
-if (answer === "A") {
-document.getElementById("result").innerHTML = "Correct!";
+if(selected == questions[currentQuestion].correct){
+
+score++;
+
+document.getElementById("feedback").innerText="Correct!";
+
+}else{
+
+document.getElementById("feedback").innerText="Incorrect.";
+
 }
-else {
-document.getElementById("result").innerHTML = "Incorrect. The answer is 90°.";
+
+currentQuestion++;
+
+if(currentQuestion < questions.length){
+
+setTimeout(()=>{
+document.getElementById("feedback").innerText="";
+loadQuestion();
+},1000);
+
+}else{
+
+document.getElementById("question").innerText="Quiz Finished!";
+document.getElementById("answers").innerHTML="";
+document.getElementById("score").innerText="Final Score: "+score+"/"+questions.length;
+
 }
 
 }
+
+loadQuestion();
